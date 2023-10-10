543711e77de33daf231e7545a41f8fbd1f1b3ab4
achuthhadnoor <armorachyuth123@gmail.com>
2023-10-10 10:17:24 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Home | MenubarGPT",
+  description: `Revolutionize Your ChatGPT Experience: Unleash the Power of Conversation from Your Mac's Menubar!`,
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
